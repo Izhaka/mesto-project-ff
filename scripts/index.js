ef04797429eb5741601bb4b2de29551cf09a5273
Izhaka <zi-na@inbox.ru>
2024-10-30 18:36:03 +0700

    add base js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const cardsContainer = document.querySelector('.places__list');
 // @todo: Функция создания карточки
 
 // @todo: Функция удаления карточки
-
+function deleteCardHandler(evt) {
+  const cardItem = evt.target.closest('.card');
+  cardItem.remove();
+};
 // @todo: Вывести карточки на страницу
+function createCard(cardObject, deleteCardHandler) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardImage.src = cardObject.link;
+  cardImage.alt = 'Фото места, описанного в карточке';
+  cardTitle.textContent = cardObject.name;
+
+  cardDeleteButton.addEventListener('click', deleteCardHandler);
+  return cardElement;
+}
+
+initialCards.forEach(cardObject => {
+  const cardItem = createCard(cardObject, deleteCardHandler);
+  cardsContainer.append(cardItem);
+})
