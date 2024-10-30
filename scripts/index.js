a65bb4bc076b8e621f458fcd8ccf77957a843266
Izhaka <zi-na@inbox.ru>
2024-10-30 18:45:06 +0700

    prettier

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,29 @@
 // @todo: Темплейт карточки
-const cardTemplate = document.querySelector('#card-template').content;
+const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
-const cardsContainer = document.querySelector('.places__list');
+const cardsContainer = document.querySelector(".places__list");
 // @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-function deleteCardHandler(evt) {
-  const cardItem = evt.target.closest('.card');
-  cardItem.remove();
-};
-// @todo: Вывести карточки на страницу
 function createCard(cardObject, deleteCardHandler) {
   const cardElement = cardTemplate.cloneNode(true);
-  const cardImage = cardElement.querySelector('.card__image');
-  const cardTitle = cardElement.querySelector('.card__title');
-  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const cardDeleteButton = cardElement.querySelector(".card__delete-button");
 
   cardImage.src = cardObject.link;
-  cardImage.alt = 'Фото места, описанного в карточке';
+  cardImage.alt = "Фото места, описанного в карточке";
   cardTitle.textContent = cardObject.name;
 
-  cardDeleteButton.addEventListener('click', deleteCardHandler);
+  cardDeleteButton.addEventListener("click", deleteCardHandler);
+
   return cardElement;
 }
-
-initialCards.forEach(cardObject => {
+// @todo: Функция удаления карточки
+function deleteCardHandler(evt) {
+  const cardItem = evt.target.closest(".card");
+  cardItem.remove();
+}
+// @todo: Вывести карточки на страницу
+initialCards.forEach((cardObject) => {
   const cardItem = createCard(cardObject, deleteCardHandler);
   cardsContainer.append(cardItem);
-})
+});
