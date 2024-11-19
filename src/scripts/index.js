fc6ed8d96af6cd55d1105bb98ad299419d551cf2
Izhaka <zi-na@inbox.ru>
2024-11-19 18:32:43 +0700

    create src folder + add recommend in index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,23 +4,24 @@ const cardTemplate = document.querySelector("#card-template").content;
 const cardsContainer = document.querySelector(".places__list");
 // @todo: Функция создания карточки
 function createCard(cardObject, deleteCardHandler) {
-  const cardElement = cardTemplate.cloneNode(true);
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   const cardImage = cardElement.querySelector(".card__image");
   const cardTitle = cardElement.querySelector(".card__title");
   const cardDeleteButton = cardElement.querySelector(".card__delete-button");
 
   cardImage.src = cardObject.link;
-  cardImage.alt = "Фото места, описанного в карточке";
+  cardImage.alt = cardObject.name;
   cardTitle.textContent = cardObject.name;
 
-  cardDeleteButton.addEventListener("click", deleteCardHandler);
+  cardDeleteButton.addEventListener("click", () => {
+    deleteCardHandler(cardElement);
+  });
 
   return cardElement;
 }
 // @todo: Функция удаления карточки
-function deleteCardHandler(evt) {
-  const cardItem = evt.target.closest(".card");
-  cardItem.remove();
+function deleteCardHandler(card) {
+  card.remove();
 }
 // @todo: Вывести карточки на страницу
 initialCards.forEach((cardObject) => {
