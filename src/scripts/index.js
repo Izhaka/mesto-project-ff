3c6503d1244f9f1b1732532c9f6ffd65f848598e
Izhaka <zi-na@inbox.ru>
2024-11-21 01:17:15 +0700

    add base logic for 1-4 points

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -30,3 +30,67 @@ initialCards.forEach((cardObject) => {
   const cardItem = createCard(cardObject, deleteCardHandler);
   cardsContainer.append(cardItem);
 });
+
+const modalArray = document.querySelectorAll('.popup');
+modalArray.forEach((modal) => {
+  const closeButton = modal.querySelector('.popup__close');
+  closeButton.addEventListener('click', () => {
+    closePopup(modal);
+  });
+
+  modal.addEventListener('mousedown', (evt) => {
+    if (evt.target.classList.contains('popup_is-opened')) {
+      closePopup(modal);
+    }
+  })
+});
+
+function openPopup(popup) {
+  popup.classList.add('popup_is-opened');
+  document.addEventListener('keydown', closePopupByEsc);
+}
+
+function closePopup(popup) {
+  popup.classList.remove('popup_is-opened');
+  document.removeEventListener('keydown', closePopupByEsc);
+}
+
+function closePopupByEsc(evt) {
+  if (evt.key === 'Escape') {
+    const openedPopup = document.querySelector('.popup_is-opened');
+    closePopup(openedPopup);
+  }
+}
+
+const profileEditButton = document.querySelector('.profile__edit-button');
+const profilePopup = document.querySelector('.popup_type_edit');
+profileEditButton.addEventListener('click', () => {
+  openPopup(profilePopup);
+  initializeProfilePopupInteractions(profilePopup);
+});
+
+const newCardButton = document.querySelector('.profile__add-button');
+const newCardPopup = document.querySelector('.popup_type_new-card');
+newCardButton.addEventListener('click', () => {
+  openPopup(newCardPopup);
+});
+
+function initializeProfilePopupInteractions (profilePopup) {
+  const formElement = profilePopup.querySelector('.popup__form');
+  const nameInput = formElement.querySelector('.popup__input_type_name');
+  const jobInput = formElement.querySelector('.popup__input_type_description');
+  const profileTitle = document.querySelector('.profile__title');
+  const profileDesc = document.querySelector('.profile__description');
+
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileDesc.textContent;
+
+  function handleFormSubmit(evt) {
+    evt.preventDefault();
+    profileTitle.textContent = nameInput.value;
+    profileDesc.textContent = jobInput.value;
+    closePopup(profilePopup);
+  }
+
+  formElement.addEventListener('submit', handleFormSubmit);
+}
